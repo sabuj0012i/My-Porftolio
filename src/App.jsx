c0aaebce461e67275c773d79524fdebf21805d90
sabuj0012i <sabujhossainraj2000@gmail.com>
2025-11-22 23:18:31 +0600

    change in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-
-import './App.css'
-import Portfolio from './Pages/portfolio'
+import React from "react";
+import "./App.css";
+import Portfolio from "./Pages/Portfolio"; 
 
 function App() {
- 
-
   return (
     <div>
-      <Portfolio/>
+      <Portfolio />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
